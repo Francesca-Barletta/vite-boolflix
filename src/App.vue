47fc4d0d4b3feb77cf4ef97cf4d47e374f5f24e4
Francesca Barletta <wallebar@gmail.com>
2024-04-16 13:10:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+// import axios from 'axios'
+// import { store } from './store.js'
+  export default {
+    
+  }
+</script>
+<template>
+  <div>
+<h1>Boolflix</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
